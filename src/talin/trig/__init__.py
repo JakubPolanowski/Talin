eff6c0ea592209945e87cede7c75f684d5def155
Jakub Polanowski <jakubpol27@gmail.com>
2022-05-28 02:08:32 -0400

    implemented trig (linear angle)

---
+++ src/talin/trig/__init__.py
@@ -0,0 +1,1 @@
+from src.talin.trig.trigonometry import *
---
+++ src/talin/trig/trigonometry.py
@@ -0,0 +1,28 @@
+from math import radians
+import numpy as np
+
+
+def linear_angle(slope, units="deg"):
+    """Returns the angle of the line specified by the slope, often referred to as Linear Regression Angle
+
+    Args:
+        slope (Number): Slope of the line
+        units (str, optional): "deg" or "rad" for either degrees or radians. Defaults to "deg".
+
+    Raises:
+        ValueError: Invalid unit specified
+
+    Returns:
+        Number: Angle of linear line in either degrees (default) or radians
+    """
+
+    if units != "deg" and units != "rad":
+        raise ValueError(
+            "Units must be either 'deg' (for degrees) or 'rad' (for radians).")
+
+    radians = np.arctan(slope)
+
+    if units == "rad":
+        return radians
+    else:
+        return np.degrees(radians)
