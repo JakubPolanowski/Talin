8737d5222d63ee96c5a9208a7f8085fbd7ea32bd
Jakub Polanowski <jakubpol27@gmail.com>
2022-05-31 03:22:25 -0400

    implemented natr

--- src/talin/indicators/volatility.py
+++ src/talin/indicators/volatility.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 """
 
-TODO IMPLEMENT
+# TODO refactor to use predominantly pandas series
+# TODO consider replacing loopback with periods 
 
 NATR                 Normalized Average True Range
 
@@ -14,6 +15,7 @@ __all__ = [
 
 
 def trange(high, low, prev_close):
+    # TODO consider if prev_close should become close
     """Calculates the True Range given the highs, lows and the previous closes (period-1). Note that inputs must be given as numpy arrays or similar objects such as pandas series. 
 
     Args:
@@ -28,6 +30,7 @@ def trange(high, low, prev_close):
 
 
 def atr(high, low, prev_close, loopback=14):
+    # TODO consider if prev_close should become close
     """Calculates the Average True Range given the highs, lows and the previous closes (period-1) with a set loopback. Note that the input arrays (high, low and prev_close) must be given as numpy arrays or similar objects such as pandas series.
 
     Args:
@@ -41,3 +44,19 @@ def atr(high, low, prev_close, loopback=14):
     """
 
     return pd.Series(trange(high, low, prev_close)).rolling(loopback).mean().values
+
+
+def natr(high: pd.Series, low: pd.Series, close: pd.Series, periods=14) -> pd.Series:
+    """Calculates the Normalized True Range.
+
+    Args:
+        high (pd.Series): Series of highs
+        low (pd.Series): Series of lows
+        close (pd.Series): Series of closes
+        periods (int, optional): N periods to loop back. Defaults to 14.
+
+    Returns:
+        pd.Series: Normalized True Range series
+    """
+
+    return atr(high, low, close.shift(1), periods) / close * 100
