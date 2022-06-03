0cee720bf45876714849fea6dc9bdfe7473257df
Jakub Polanowski <jakubpol27@gmail.com>
2022-06-02 20:42:41 -0400

    reworked tests on basis that inputs and outputs should be pd series

--- tests/test_simple_stats.py
+++ tests/test_simple_stats.py
@@ -1,38 +1,39 @@
 from src.talin.stats import simple_stats
 import numpy as np
+import pandas as pd
 
-array = np.random.rand(10)
+series = pd.Series(np.random.rand(100))
 
 
 def test_mean():
-    assert simple_stats.average(array) == np.mean(array)
+    assert simple_stats.average(series) == series.mean()
 
 
 def test_median():
-    assert simple_stats.median(array) == np.median(array)
+    assert simple_stats.median(series) == series.median()
 
 
 def test_var():
-    assert simple_stats.var(array) == np.var(array, ddof=1)
+    assert simple_stats.var(series) == series.var()  # note default is ddof=1
 
 
 def test_var_ddof():
     for ddof in range(6):
-        assert simple_stats.var(array, ddof=ddof) == np.var(array, ddof=ddof)
+        assert simple_stats.var(series, ddof=ddof) == series.var(ddof=ddof)
 
 
 def test_std():
-    assert simple_stats.std(array) == np.std(array, ddof=1)
+    assert simple_stats.std(series) == series.std()  # note default is ddof=1
 
 
 def test_std_ddof():
     for ddof in range(6):
-        assert simple_stats.std(array, ddof=ddof) == np.std(array, ddof=ddof)
+        assert simple_stats.std(series, ddof=ddof) == series.std(ddof=ddof)
 
 
 def test_typical_price():
-    assert simple_stats.typical_price(10, 5, 3) == 6
+    assert simple_stats.typical_price(10, 5, 3).values[0] == 6
 
 
 def test_weighted_close():
-    assert simple_stats.weighted_close(10, 5, 3) == 5.25
+    assert simple_stats.weighted_close(10, 5, 3).values[0] == 5.25
